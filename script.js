c713693e00d37b7d45a2aa609186d9b45d42a23e
Helen-122 <helene.mazourine@gmail.com>
2021-04-11 20:42:13 +0200

    js part 3 ex3

---
+++ script.js
@@ -0,0 +1,21 @@
+var text = document.getElementById("text");
+
+//Pour afficher le texte en bleu:
+function blueText(){
+  text.style.color = "#3C73E7";
+}
+
+//Pour afficher le texte en vert:
+function greentext(){
+  text.style.color = "#18B55F";
+}
+
+//Pour afficher le texte en rouge:
+function redText(){
+  text.style.color = "#E73C4D";
+}
+
+//Pour afficher le texte en bleu:
+function displayBlock(){
+  text.style.display = "#3C73E7";
+}
